69a835ee99e525563da9c7b31ded922c3b3b0e95
johnchicKR <johnmilk@naver.com>
2025-11-06 12:09:13 +0900

    Upload Unity2Dplatformer
    Unity 2d Project Back Up

---
+++ Assets/Scripts/Mechanics/VictoryZone.cs
@@ -0,0 +1,27 @@
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// Marks a trigger as a VictoryZone, usually used to end the current game level.
+    /// </summary>
+    public class VictoryZone : MonoBehaviour
+    {
+        public GameObject thankyouText;
+
+        public Animator anim;
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            var p = collider.gameObject.GetComponent<PlayerController>();
+            if (p != null)
+            {
+                thankyouText.SetActive(true);
+                var ev = Schedule<PlayerEnteredVictoryZone>();
+                ev.victoryZone = this;
+                anim.SetBool("Victory", true);
+            }
+        }
+    }
+}
